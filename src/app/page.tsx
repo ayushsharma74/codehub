6c222b2ae3408fe94e7f4d8c47ff6da858d5093f
Ayush Sharma <ayush.sharma74892@gmail.com>
2024-12-15 16:32:26 +0530

    logo - (not final)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ export default function Home() {
   return (
     <main className="bg-black">
       <FlickeringGridRounded />
-      {/* <LandingContent /> */}
     </main>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,8 +9,9 @@ import Image from 'next/image'
 const Navbar = () => {
   return (
     <nav className='bg-black flex border-b-2 border-zinc-900  justify-between items-center p-2'>
-        <div className='text-white'>
-            <h1 className='font-bold text-3xl ml-2'>Logo.</h1>
+        <div className='text-white flex gap-2'>
+            <h1 className='font-semibold text-2xl ml-2'>CodeHub</h1>
+            <Image src={"/coffee-svgrepo-com.svg"} alt='logo' width={25} height={25} />
         </div>
         <div className='text-white'>
             <RainbowButton className='flex gap-2 font-medium'>Star on GitHub <Image src={"/star-svgrepo-com.svg"} width={20} height={20} alt='star' /></RainbowButton>
