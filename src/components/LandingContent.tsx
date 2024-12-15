01ce254d6935d06448b4b43dce79bb510cee6534
Ayush Sharma <ayush.sharma74892@gmail.com>
2024-12-15 16:51:47 +0530

    font changes and some effects

--- src/components/LandingContent.tsx
+++ src/components/LandingContent.tsx
@@ -3,6 +3,10 @@ import React from "react";
 // import AnimatedShinyText from "./ui/animated-shiny-text";
 import { AnimatedShinyTextDemo } from "./LandingShinyText";
 import { RainbowButton } from "./ui/rainbow-button";
+import WordFadeIn from "./ui/word-fade-in";
+import {Roboto} from "next/font/google"
+
+const roboto = Roboto({weight: "500", subsets: ["latin"]})
 
 type Props = {
     HeadingText: string,
@@ -14,7 +18,10 @@ const LandingContent = (props: Props) => {
   return (
     <div className="flex items-center gap-4 justify-center flex-col text-white z-10 absolute top-1/3 left-1/2 -translate-x-1/2 -translate-y-1/2">
         <AnimatedShinyTextDemo/>
-      <h1 className="text-7xl font-bold text-center">{props.HeadingText}</h1>
+      {/* <h1 className={` ${roboto.className} text-7xl text-center`}> */}
+        <WordFadeIn words={props.HeadingText} className={` ${roboto.className} text-7xl text-center text-white`} />
+        
+        {/* </h1> */}
       <p className="text-center font-semibold">
         {props.SubHeadingText}
       </p>
